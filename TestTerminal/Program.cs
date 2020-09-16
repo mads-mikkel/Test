67cad452c6bb15d0d2909169e384146ee738a7e4
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-09-16 08:35:26 +0200

    Add project files.

---
+++ TestTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
